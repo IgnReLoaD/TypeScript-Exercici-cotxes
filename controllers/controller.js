5364dea91f1134109227e28e86eddf5fcdec0f56
IgnReLoaD <ortiz.ignasi@gmail.com>
2021-04-03 00:41:34 +0200

    Controller TS no accepta getElementById

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,8 +1,37 @@
-"use strict";
-function createCar(plate, brand, color) {
-    var car = new Car(plate, color, brand);
-    car.addWheel(new Wheel(2, "SEAT"));
-    document.body.innerText = "CAR: PLATE: " + car.plate
-        + " COLOR: " + car.color + " BRAND: " + brand
-        + " WHEELS: " + JSON.stringify(car.wheels);
+// variable global tal com s'aconsella a l'enunciat,
+//   per tal que sigui accessible des de qualsevol funció
+var car;
+// codi original donat
+// function createCar(plate:string,brand:string,color:string){
+//     let car=new Car(plate,color,brand);
+//     car.addWheel(new Wheel(2,"SEAT"));
+//     document.body.innerText="CAR: PLATE: " + car.plate 
+//     + " COLOR: " +car.color + " BRAND: " + brand 
+//     + " WHEELS: " + JSON.stringify(car.wheels);
+// }
+// codi refactoritzat
+function createCar() {
+    var newCar = getDatos();
+    var valCar = valDatos(newCar);
+    valCar ? mostrar(newCar) : msgError();
+}
+function getDatos() {
+    var _a;
+    var matri = toString((_a = document.getElementById("inpPlate")) === null || _a === void 0 ? void 0 : _a.nodeValue);
+    var marca = document.getElementById("inpBrand").value;
+    var color = document.getElementById("inpColor");
+    var arrCar = [matri, marca, color];
+    return arrCar;
+}
+function valDatos(arrCar) {
+    var bolValidated = false;
+    return bolValidated;
+}
+function mostrar(arrCar) {
+    console.log(arrCar.map());
+    // document.getElementById("mostrarDatos").innerHTML = "Dades del cotxe son: " + arrCar.map();
+}
+function msgError() {
+    console.log("Dades incorrectes");
+    // document.getElementById("mostrarDatos").innerHTML = "Dades cotxe incorrectes.";
 }
--- controllers/controller.ts
+++ controllers/controller.ts
@@ -1,9 +1,44 @@
+// variable global tal com s'aconsella a l'enunciat,
+//   per tal que sigui accessible des de qualsevol funció
+let car:Car;
 
-function createCar(plate:string,brand:string,color:string){
-    let car=new Car(plate,color,brand);
-    car.addWheel(new Wheel(2,"SEAT"));
-    document.body.innerText="CAR: PLATE: " + car.plate 
-    + " COLOR: " +car.color + " BRAND: " + brand 
-    + " WHEELS: " + JSON.stringify(car.wheels);
+// codi original donat
+// function createCar(plate:string,brand:string,color:string){
+//     let car=new Car(plate,color,brand);
+//     car.addWheel(new Wheel(2,"SEAT"));
+//     document.body.innerText="CAR: PLATE: " + car.plate 
+//     + " COLOR: " +car.color + " BRAND: " + brand 
+//     + " WHEELS: " + JSON.stringify(car.wheels);
+// }
+
+
+// codi refactoritzat
+function createCar() {
+    let newCar:any     = getDatos();
+    let valCar:boolean = valDatos(newCar);
+    valCar ? mostrar(newCar) : msgError();
 }
 
+function getDatos():any {
+    let matri: string   = toString(document.getElementById("inpPlate")?.nodeValue);
+    let marca: string   = document.getElementById("inpBrand").value; 
+    let color: string   = document.getElementById("inpColor");
+    let arrCar:string[] = [matri,marca,color];
+    return arrCar;
+}
+
+function valDatos(arrCar:any):boolean {
+    let bolValidated:boolean = false;
+
+    return bolValidated;
+}
+
+function mostrar(arrCar:any):void {
+    console.log(arrCar.map());
+    // document.getElementById("mostrarDatos").innerHTML = "Dades del cotxe son: " + arrCar.map();
+}
+
+function msgError():void {
+    console.log("Dades incorrectes");
+    // document.getElementById("mostrarDatos").innerHTML = "Dades cotxe incorrectes.";
+}
